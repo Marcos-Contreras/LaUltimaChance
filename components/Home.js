1c4cf521bec8cc6c76f084dd9b7b6334aad29920
Marcos-Contreras <marcosalonso2230@gmail.com>
2022-04-12 00:38:11 -0500

    Primer commit

---
+++ components/Home.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//IMPORTANDO NATIVEBASE
+import { NativeBaseProvider, Text, Button, Box } from 'native-base';
+
+function Home ({ navigation }) {
+    return (
+      <NativeBaseProvider>
+        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+          <Text>Pantalla de inicio</Text>
+          <Button onPress={() => navigation.navigate('Login')}>Ir a Login</Button>
+        </Box>
+      </NativeBaseProvider>
+    );
+  }
+
+
+export default Home;
---
+++ components/Login.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//IMPORTANDO NATIVEBASE
+import { NativeBaseProvider, Text, Button, Box } from 'native-base';
+
+function Login ({ navigation }) {
+    return (
+      <NativeBaseProvider>
+        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+          <Text>Login</Text>
+          <Button onPress={() => navigation.navigate('Card')}>Ir a Card</Button>
+        </Box>
+      </NativeBaseProvider>
+    );
+  }
+
+
+export default Login;
